18ddc95208d6206b88d9f1cd9aa23bdf5adb682f
vincendep <vincenzo.depetris@student.univaq.it>
2019-07-08 17:11:21 +0200

    added test services

---
+++ src/app/services/alimento.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {Alimento} from '../model/alimento.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlimentoService {
+  alimenti: Alimento[] = [];
+
+  constructor() {
+    const riso: Alimento = new Alimento();
+    riso.nome = 'riso';
+    riso.calorie = 100;
+    riso.carboidrati = 50;
+    riso.grassi = 15;
+    riso.proteine = 15;
+
+    const manzo: Alimento = new Alimento();
+    manzo.nome = 'manzo';
+    manzo.calorie = 100;
+    manzo.proteine = 70;
+    manzo.grassi = 35;
+    manzo.carboidrati = 15;
+
+
+    this.alimenti.push(riso, manzo);
+  }
+
+  getAll(): Alimento[] {
+    return this.alimenti;
+  }
+
+  add(a: Alimento) {
+    this.alimenti.push(a);
+  }
+}
+
+
