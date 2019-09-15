aad58cd4143e6de0e1707426c16264cfd88593e8
vincendep <vincenzo.depetris@student.univaq.it>
2019-09-15 18:54:35 +0200

    Add toast message in profilo page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -68,5 +68,4 @@ export class LoginPage implements OnInit {
             this.loginTitle = data;
         });
     }
-
 }
--- src/app/pages/profilo/profilo.page.ts
+++ src/app/pages/profilo/profilo.page.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UtenteService} from '../../services/utente.service';
 import {Utente} from '../../model/utente.model';
-import {PopoverController} from '@ionic/angular';
+import {PopoverController, ToastController} from '@ionic/angular';
 import {TranslateService} from '@ngx-translate/core';
 import {ProfiloMenuComponent} from '../../components/profilo-menu/profilo-menu.component';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
@@ -16,14 +16,17 @@ export class ProfiloPage implements OnInit {
     private utente: Utente;
     private profiloFormModel: FormGroup;
     private showSelect = true;
+    private toastMessage: string;
 
     constructor(private translateService: TranslateService,
                 private utenteService: UtenteService,
                 public popoverController: PopoverController,
-                private formBuilder: FormBuilder) {
+                private formBuilder: FormBuilder,
+                private toastController: ToastController) {
     }
 
     ngOnInit() {
+        this.initTranslate();
         this.profiloFormModel = this.formBuilder.group({
             nome: ['', Validators.compose([
                 Validators.required
@@ -57,7 +60,6 @@ export class ProfiloPage implements OnInit {
         });
     }
 
-    // TODO le select non si aggiornano automaticamente al cambio di lingua
     ionViewWillEnter() {
         this.resetFormFields();
     }
@@ -72,7 +74,9 @@ export class ProfiloPage implements OnInit {
         this.utente.informazioniFisiche[this.utente.informazioniFisiche.length - 1].altezza = this.profiloFormModel.value.altezza;
         this.utente.informazioniFisiche[this.utente.informazioniFisiche.length - 1].peso = this.profiloFormModel.value.peso;
 
-        this.utenteService.updateProfilo(this.utente).subscribe();
+        this.utenteService.updateProfilo(this.utente).subscribe(() => {
+            this.showToastConfirmation();
+        });
     }
 
     resetFormFields() {
@@ -92,6 +96,14 @@ export class ProfiloPage implements OnInit {
         });
     }
 
+    async showToastConfirmation() {
+        const toast = await this.toastController.create({
+            message: this.toastMessage,
+            duration: 2000
+        });
+        toast.present();
+    }
+
     async presentMenu(ev: any) {
         const popover = await this.popoverController.create({
             component: ProfiloMenuComponent,
@@ -100,5 +112,11 @@ export class ProfiloPage implements OnInit {
         });
 
         return await popover.present();
+    }
+
+    private initTranslate() {
+        this.translateService.get('PROFILO_AGGIORNATO').subscribe((data) => {
+            this.toastMessage = data;
+        });
     }
 }
